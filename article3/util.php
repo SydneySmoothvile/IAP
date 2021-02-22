e2a920b6e7cee3226048f2512fd4df7556b68f23
SydneySmoothvile <69866777+SydneySmoothvile@users.noreply.github.com>
2021-02-22 17:25:39 +0300

    Add files via upload

---
+++ article3/util.php
@@ -0,0 +1,14 @@
+<?php
+      class Util{
+ 
+          //About DB 
+          //your databae name here
+          static $DB_NAME = "myapp";
+          //your database user here
+          static $DB_USER = "root";
+          //password for database user here
+          static $DB_USER_PASS = "";
+          //machine that is holding your database
+          static $SERVER_NAME = "localhost:3307";
+      }
+?>
